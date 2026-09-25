7aedeb31ea3be0715e8dace48fd13e39895db8a5
muhammadusmanjamali <usmanhyderi54@gmail.com>
2026-09-25 21:37:18 +0500

    Fix database connection

--- Backend Files/database.py
+++ Backend Files/database.py
@@ -5,18 +5,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 load_dotenv()
 
+MYSQLHOST = os.getenv("MYSQLHOST")
+MYSQLPORT = os.getenv("MYSQLPORT")
+MYSQLUSER = os.getenv("MYSQLUSER")
+MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
+MYSQLDATABASE = os.getenv("MYSQLDATABASE")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
+DATABASE_URL = (
+    f"mysql+pymysql://{MYSQLUSER}:{MYSQLPASSWORD}"
+    f"@{MYSQLHOST}:{MYSQLPORT}/{MYSQLDATABASE}"
+)
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -24,16 +29,12 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-
 Base = declarative_base()
 
 
 def get_db():
-
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
